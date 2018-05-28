61019cd9dddac21ec4cfe0013948b2d1406decdd
cfregly <chris@fregly.com>
2018-05-28 00:35:51 -0700

    updated

--- docs/push.sh
+++ docs/push.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-pip uninstall -y mkdocs-material
-pip install ./mkdocs-material/
-pip install markdown-include
-pip install pygments
+#pip install mkdocs
+#pip uninstall -y mkdocs-material
+#pip install ./mkdocs-material/
+#pip install markdown-include
+#pip install pygments
 
 rm -rf generated/
 
-mkdocs gh-deploy
+mkdocs gh-deploy 
--- docs/src/api/index.md
+++
@@ -1,7 +0,0 @@
-<html>
-<head>
-<meta http-equiv="refresh" content="0; url=http://github.com/PipelineAI/predict/" />
-</head>
-<body>
-</body>
-</html>
